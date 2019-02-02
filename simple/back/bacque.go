3057aae80632aebfa59db44eafed79563a141508
Matt Davis <maroda@gmail.com>
2019-02-01 16:04:36 -0800

    prometheus endpoints and basic count metrics

--- simple/back/bacque.go
+++ simple/back/bacque.go
@@ -6,7 +6,7 @@
 	/fetch - retrieves the localtime from the containing OS
 	/ping - a liveliness check
 
-	Version = Bv003
+	Version = Bv004
 
 */
 
@@ -17,13 +17,30 @@ import (
 	"net/http"
 	"os/exec"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
+// define prometheus metrics
+var fetchCount = prometheus.NewCounter(prometheus.CounterOpts{
+	Name: "bacque_fetch_total",
+	Help: "Total number of requests for DateTime.",
+})
+var rootCount = prometheus.NewCounter(prometheus.CounterOpts{
+	Name: "bacque_root_total",
+	Help: "Total number of requests for /.",
+})
+var pingCount = prometheus.NewCounter(prometheus.CounterOpts{
+	Name: "bacque_ping_total",
+	Help: "Total number of requests for Readiness /ping.",
+})
+
 // API call that returns local system datetime
 func fetch(w http.ResponseWriter, r *http.Request) {
 	// access a local command and return its output
+	fetchCount.Add(1)
 	app := "date"
 	cmd := exec.Command(app)
 	stdout, err := cmd.Output()
@@ -44,6 +61,7 @@ func fetch(w http.ResponseWriter, r *http.Request) {
 
 // liveliness check
 func ping(w http.ResponseWriter, r *http.Request) {
+	pingCount.Add(1)
 	w.Write([]byte("pong\n"))
 	zerolog.TimeFieldFormat = ""
 	log.Info().
@@ -56,10 +74,15 @@ func ping(w http.ResponseWriter, r *http.Request) {
 		Msg("")
 }
 
-// print Hello and the request path
-// if there is no valid endpoint, it will always default here
 func main() {
+	prometheus.MustRegister(fetchCount)
+	prometheus.MustRegister(rootCount)
+	prometheus.MustRegister(pingCount)
+
+	// print Hello and the request path
+	// if there is no valid endpoint, it will always default here
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rootCount.Add(1)
 		fmt.Fprintf(w, "Hello. %s\n", r.URL.Path)
 		zerolog.TimeFieldFormat = ""
 		log.Info().
@@ -74,8 +97,12 @@ func main() {
 
 	// fetch local command output
 	http.HandleFunc("/fetch", fetch)
+
 	// ping ::: liveliness check that returns 'pong'
 	http.HandleFunc("/ping", ping)
+
+	// m ::: prometheus metrics endpoint (can this be logged?)
+	http.Handle("/m", promhttp.Handler())
 
 	// start server
 	if err := http.ListenAndServe(":9999", nil); err != nil {
